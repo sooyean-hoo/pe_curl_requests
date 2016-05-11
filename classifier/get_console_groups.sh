068786b4814ec7f6cc33b1cbcde0e2f820604577
Gary Larizza <gary@puppetlabs.com>
2016-05-11 10:07:13 -0700

    Update get_console_groups.sh

--- classifier/get_console_groups.sh
+++ classifier/get_console_groups.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CONSOLE='split-console.puppetlabs.vm'
+CONSOLE='split-console'
 
-curl -k -X GET https://${CONSOLE}:4433/rbac-api/v1/groups \
+curl -k -X GET https://${CONSOLE}:4433/classifier-api/v1/groups \
   --cert $(puppet config print hostcert) \
   --key $(puppet config print hostprivkey) \
   --cacert $(puppet config print localcacert) \
