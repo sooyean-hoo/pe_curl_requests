e478f331bc013ae9e611eb061f93314187c00263
Gary Larizza <gary@puppetlabs.com>
2016-05-11 10:01:16 -0700

    Add some more curl calls

--- console/get_console_groups.sh
+++ console/get_console_groups.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONSOLE='split-console.puppetlabs.vm'
 
-curl -k -X GET https://${CONSOLE}:4433/rbac-api/v1/ds \
+curl -k -X GET https://${CONSOLE}:4433/rbac-api/v1/groups \
   --cert $(puppet config print hostcert) \
   --key $(puppet config print hostprivkey) \
   --cacert $(puppet config print localcacert) \
--- puppetserver/rbac/get_user_groups.sh
+++
@@ -1,7 +0,0 @@
-
-
-curl -k -X GET https://localhost:4433/rbac-api/v1/groups \
-  --cert /etc/puppetlabs/puppet/ssl/certs/pemaster.puppetlabs.vm.pem \
-  --key /etc/puppetlabs/puppet/ssl/private_keys/pemaster.puppetlabs.vm.pem \
-  --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem \
-  -H "Content-Type: application/json"
