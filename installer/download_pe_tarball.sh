267e4f4611604393ea2e90e0d7e4eee7b56c4e4d
Gary Larizza <gary@puppetlabs.com>
2016-08-08 12:50:29 -0700

    Document the installer script

--- installer/download_pe_tarball.sh
+++ installer/download_pe_tarball.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script will download the requested version of PE from S3. It will
 # also resume broken downloads to save time and rename the resultant file
+
+# INSTALLER CHOICES #
+# Either pass these environment variables inline or modify the default
+# values (note, it's the value after the ':-' but before the close curly brace }
 DOWNLOAD_DIST=${DOWNLOAD_DIST:-el}
 DOWNLOAD_RELEASE=${DOWNLOAD_RELEASE:-7}
 DOWNLOAD_ARCH=${DOWNLOAD_ARCH:-x86_64}
