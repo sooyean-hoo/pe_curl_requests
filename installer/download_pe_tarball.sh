202b9340630f1de9e10348450e7e8538bfe34eab
Gary Larizza <gary@puppetlabs.com>
2016-08-08 12:48:25 -0700

    Modify download script to look for EnvVars
    This commit adds the ability to pass environmental variables to the installer
    download script

--- installer/download_pe_tarball.sh
+++ installer/download_pe_tarball.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script will download the requested version of PE from S3. It will
 # also resume broken downloads to save time and rename the resultant file
-DOWNLOAD_DIST='el'
-DOWNLOAD_RELEASE='7'
-DOWNLOAD_ARCH='x86_64'
-DOWNLOAD_VERSION='latest'
+DOWNLOAD_DIST=${DOWNLOAD_DIST:-el}
+DOWNLOAD_RELEASE=${DOWNLOAD_RELEASE:-7}
+DOWNLOAD_ARCH=${DOWNLOAD_ARCH:-x86_64}
+DOWNLOAD_VERSION=${DOWNLOAD_VERSION:-latest}
 
 curl -L \
   -o "./puppet_enterprise_${DOWNLOAD_DIST}_${DOWNLOAD_RELEASE}_${DOWNLOAD_ARCH}_${DOWNLOAD_VERSION}.tar.gz" \
