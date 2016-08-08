3d23625188523b58df4792c63dcb31e5f20db0e0
Gary Larizza <gary@puppetlabs.com>
2016-08-08 12:38:05 -0700

    Script to download PE
    This script should save me lots of time on-site!

---
+++ installer/download_pe_tarball.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script will download the requested version of PE from S3. It will
+# also resume broken downloads to save time and rename the resultant file
+DOWNLOAD_DIST='el'
+DOWNLOAD_RELEASE='7'
+DOWNLOAD_ARCH='x86_64'
+DOWNLOAD_VERSION='latest'
+
+curl -L \
+  -o "./puppet_enterprise_${DOWNLOAD_DIST}_${DOWNLOAD_VERSION}_${DOWNLOAD_ARCH}_${DOWNLOAD_VERSION}.tar.gz" \
+  -C - \
+  "https://pm.puppetlabs.com/cgi-bin/download.cgi?dist=${DOWNLOAD_DIST}&rel=${DOWNLOAD_RELEASE}&arch=${DOWNLOAD_ARCH}&ver=${DOWNLOAD_VERSION}"
+
