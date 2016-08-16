45e62a8274feb5cb35d7b813f2ad0eced97e067c
Gary Larizza <gary@puppetlabs.com>
2016-08-16 16:04:56 -0700

    Add V3 query

--- puppetdb/get_node_record.sh
+++ puppetdb/get_node_record.sh
@@ -9,3 +9,5 @@ curl -X GET \
   --key    $(puppet config print hostprivkey) \
   --cacert $(puppet config print localcacert) \
   https://${PUPPETDB}:8081/pdb/query/v4/nodes/${NODE}
+  # VERSION 3 QUERY BELOW
+  # https://${PUPPETDB}:8081/v3/nodes/${NODE}
