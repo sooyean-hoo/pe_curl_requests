f614bae750b05169d278a9cc31bbc59318b0215c
Trev Harmon <trevharmon@gmail.com>
2016-09-16 16:10:15 -0600

    add additional node group scripts
    These scripts are for retrieving node group information from the
    classification API endpoint in a usable manner.

---
+++ classifier/get_node_group_parent_id.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONSOLE=$(puppet config print server)
+NODE_GROUP=$1
+RUBY=/opt/puppetlabs/puppet/bin/ruby
+
+curl -s -X GET https://${CONSOLE}:4433/classifier-api/v1/groups \
+  --cert $(puppet config print hostcert) \
+  --key $(puppet config print hostprivkey) \
+  --cacert $(puppet config print localcacert) \
+  -H "Content-Type: application/json" \
+  | $RUBY -p -e "gsub(/^(\[.*|.+},)({.+\"name\":\"${NODE_GROUP}\".+?}})(,{|\]).+$/, \"\\\\2\")" \
+  | $RUBY -p -e 'gsub(/^.+"parent":"(.+?)".+$/, "\\1")'
+
