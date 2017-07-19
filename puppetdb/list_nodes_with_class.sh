659f822a6a62d5e946ee14f70d415d0d19f34e90
Gabriel M. Schuyler <gabe@puppet.com>
2017-07-18 20:43:19 -0700

    Update to take class name from argv (#19)
    * Update to take class name from argv

--- puppetdb/list_nodes_with_class.sh
+++ puppetdb/list_nodes_with_class.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+#
+# This shell script makes it easy to see what nodes are applying a class, given
+# on the command line.  For instance, to see what nodes have profile::base
+#
+#      ./list_nodes_with_class.sh 'Profile::Base'
 
 SET_SERVER=$(puppet config print server)
 PUPPETDB="${PUPPETDB:-$SET_SERVER}"
+CLASS_NAME=$1
 
 curl -X GET \
   --tlsv1 \
   --stderr /dev/null \
-  --data-urlencode 'query=["and",["=","type","Class"],["=","title","Profile::Base"]]' \
+  --data-urlencode "query=[\"and\",[\"=\",\"type\",\"Class\"],[\"=\",\"title\",\"${CLASS_NAME}\"]]" \
   --cert   $(puppet config print hostcert) \
   --key    $(puppet config print hostprivkey) \
   --cacert $(puppet config print localcacert) \
