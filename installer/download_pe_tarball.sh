0b0225d4446d2b07a4aefb82b4336edf656dc5b0
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-08-13 23:41:17 +0800

    Change to ospversion

--- installer/download_pe_tarball.sh
+++ installer/download_pe_tarball.sh
@@ -1377,12 +1377,12 @@ __END
       
       opsversion=( ${DOWNLOAD_VERSION//./ })
       
-      if [ "$opensourcepuppetversion" = "L" ] ; then
-        opensourcepuppetversion="" ;
+      if [ "$ospversion" = "L" ] ; then
+        ospversion="" ;
       fi;
-      opensourcepuppetversion_def=${opsversion[1]} ;
-      opensourcepuppetversion=${opensourcepuppetversion:-$opensourcepuppetversion_def} ;
-      opsversion1="puppet${opensourcepuppetversion}" ;
+      ospversion_def=${opsversion[1]} ;
+      ospversion=${ospversion:-$ospversion_def} ;
+      opsversion1="puppet${ospversion}" ;
 
 
       if [ "$DOWNLOAD_DIST" = "ubuntu" ] ; then
